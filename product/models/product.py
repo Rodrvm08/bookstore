f717a8eca422454bc3af7f6443a830fbb604eaa2
Rodrigo Vilela Magalhães <rodrvm08@gmail.com>
2025-11-24 17:01:00 -0300

    serializers

--- product/models/product.py
+++ product/models/product.py
@@ -3,7 +3,7 @@ from django.db import models
 from product.models import Category
 
 class Product(models.Model):
-    titles = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     description = models.TextField(max_length=500, blank=True, null=True)
     price = models.PositiveIntegerField(null=True)
     active = models.BooleanField(default=True)
