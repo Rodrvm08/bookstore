eaeb9815edd3ef979df07b27fc9e24a0508c19eb
Rodrigo Vilela Magalhães <rodrvm08@gmail.com>
2025-12-17 15:48:06 -0300

    correção

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -150,4 +150,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
+ALLOWED_HOSTS = []
+if "DJANGO_ALLOWED_HOSTS" in os.environ:
+    ALLOWED_HOSTS = os.environ["DJANGO_ALLOWED_HOSTS"].split(" ")
+else:
+    # Valor padrão para o PythonAnywhere
+    ALLOWED_HOSTS = ['rodrvm08.pythonanywhere.com']
